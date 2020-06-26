8d39d0b13fb45662bdf691642ae8e1fb50701e6c
sagargandhi25 <gandhisagar25@gmail.com>
2020-06-26 09:54:49 +0100

    Navigation 2 updated

--- app/src/main/java/com/fes/myapplication4/Navigation2.java
+++ app/src/main/java/com/fes/myapplication4/Navigation2.java
@@ -2,5 +2,8 @@ package com.fes.myapplication4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.maps.GoogleMap;
+
 public class Navigation2 extends AppCompatActivity {
+    private GoogleMap mMap;
 }
